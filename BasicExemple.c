d62c3dbb4eeaf3c22e9abf296eee4710d6ced281
Br1s3 <maxime.elisabeth@etu.unilim.fr>
2026-08-20 20:19:20 +0200

    Update

--- BasicExemple.c
+++ BasicExemple.c
@@ -1,19 +1,16 @@
-#define GLIPH_PADDING
 #define GLIPH_IMPLEMENTATION
 #include "gliph.h"
 
 #define HEIGHT (9*2)
-#define WIDTH (16*2*2)
+#define WIDTH (16*2)
 
 int main()
 {
     GLIPH_ALLOC(console, WIDTH, HEIGHT);
     ConsoleClear(console, ' ');
     PrintRectangle(console, 0, 0, WIDTH, HEIGHT, '#');
-    // PrintRectangle(console, WIDTH/4+1, 1, (WIDTH/2-2), HEIGHT-2, ' ');
     PrintRectangle(console, 1, 1, WIDTH-2, HEIGHT-2, ' ');
     PrintCircle(console, WIDTH/2, HEIGHT/2, 5, '@');
     PrintConsole(console);
-    // GLIPH_FREE2D(console, HEIGHT);
     return 0;
 }
